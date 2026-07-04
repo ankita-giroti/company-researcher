4f68efcbffa4d17a5c4856a6df5f07c9281adef3
Ankita Giroti <ankitagiroti@gmail.com>
2026-07-05 00:44:37 +0530

    Updated app.js

--- template/app.js
+++ template/app.js
@@ -1,5 +1,4 @@
-// ---- Configuration ----
-const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = "";
 
 // ---- Elements ----
 const reportForm = document.getElementById("reportForm");
